4852c0d4a24074be66486b017782a3382217b31d
Azarela-Vkn <76985154+Azarela-Vkn@users.noreply.github.com>
2021-01-05 12:24:44 +0700

    index.js

--- index.js
+++ index.js
@@ -18,12 +18,9 @@ const arrayBulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Jul
 const bulan = arrayBulan[moment().format('MM') - 1]
 
 const config = {
-    A187: 'NAMA BIT KALIAN🤖',
-    instagram: 'https://instagram.com/sensi.edtz_',
-    nomer: 'wa.me/+6285964224017',
-    aktif: 'TERSERAH KALIAN MAU JAM BRP AKTIFNYA',
-    youtube: 'https://youtube.sensi chanel.com',
-    whatsapp: 'terserah kalian taruh group yg mana',
+    A187: 'Azarela Von'Earlshide',
+    nomer: 'wa.me/+62089652574706',
+    aktif: 'jika ada kuota',
     tanggal: `TANGGAL: ${moment().format('DD')} ${bulan} ${moment().format('YYYY')}`,
     waktu: time
 }
